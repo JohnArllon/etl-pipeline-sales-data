180ff43c6de27ecd6a93d43584cb2fb641c78b4a
John Arllon Batista <johnarllon21@gmail.com>
2026-03-31 16:27:12 -0300

    Add .gitignore and improve transform

--- src/transform.py
+++ src/transform.py
@@ -1,15 +1,20 @@
 def transform_data(df):
     print("🔄 Transformando dados...")
 
+    # remover nulos
     df = df.dropna()
 
+    # converter tipos
     df["price"] = df["price"].astype(float)
     df["quantity"] = df["quantity"].astype(int)
 
+    # nova coluna
     df["total"] = df["price"] * df["quantity"]
-    
+
+    # 👉 NOVO: salvar dados tratados
     df.to_csv("data/processed/sales_clean.csv", index=False)
 
+    # 👉 NOVO: mostrar quantidade de linhas
     print(f"✅ Linhas finais: {len(df)}")
-    
+
     return df
